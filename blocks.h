7955f668e693049d9ffea6192ff6c4f24313ee84
Artur Dobrowolski <artur.dobrowolski@startpage.com>
2021-03-07 16:26:08 +0100

    Changed script names to suit new scriptset

--- blocks.h
+++ blocks.h
@@ -2,18 +2,18 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/				/*Update Interval*/	/*Update Signal*/
 //	{"",		"~/.local/bin/statusbar/mpc",		1,			12},
-//	{"",		"$LOCAL_BIN_STATUSBAR/ethernet",	2,			4},
-//	{"",		"$LOCAL_BIN_STATUSBAR/wifi",		2,			5},
-//	{"",		"$LOCAL_BIN_STATUSBAR/internet",	1200,			7},
-//	{"",		"$LOCAL_BIN_STATUSBAR/cputemp",		2,			13},
-//	{"",		"$LOCAL_BIN_STATUSBAR/disk",		5,			16},
-//	{"",		"$LOCAL_BIN_STATUSBAR/mem",		2,			14},
-//	{"",		"$LOCAL_BIN_STATUSBAR/battery",		2,			6},
-//	{"",		"$LOCAL_BIN_STATUSBAR/feed",		0,			15},
-//	{"",		"$LOCAL_BIN_STATUSBAR/updates",		0,			2},
-//	{"",		"$LOCAL_BIN_STATUSBAR/brightness",	0,			11},
-//	{"",		"$LOCAL_BIN_STATUSBAR/volume",		2,			10},
-//	{"",		"$LOCAL_BIN_STATUSBAR/clock",		2,			1},
+	{"",		"statusbar-ethernet",	2,			4},
+//	{"",		"statusbar-wifi",		2,			5},
+//	{"",		"statusbar-internet",	1200,			7},
+//	{"",		"statusbar-cputemp",		2,			13},
+	{"",		"statusbar-disk",		5,			16},
+	{"",		"statusbar-memory",		2,			14},
+//	{"",		"statusbar-battery",		2,			6},
+	{"",		"statusbar-feed",		0,			15},
+	{"",		"statusbar-updates",		0,			2},
+//	{"",		"statusbar-brightness",	0,			11},
+	{"",		"statusbar-volume",		2,			10},
+	{"",		"statusbar-clock",		2,			1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
