e682e7a2d86e3d4b267230a28451ae7ea92ecda3
Artur Dobrowolski <artur.dobrowolski@startpage.com>
2021-03-07 14:49:35 +0100

    Applied patch dwmblocks-statuscmd: dwm integration Patch moved to patches/ subdir Also tweaked settings in blocks.h (all commented out)

--- blocks.h
+++ blocks.h
@@ -1,20 +1,20 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "cat ~/.pacupdate | sed /📦0/d",					0,		9},
-	
-	{"🧠", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "~/bin/statusbar/volume",						0,		10},
-
-	{"☀", "xbacklight | sed 's/\\..*//'",					0,		11},
-	
-	{"", "~/bin/statusbar/battery",						5,		0},
-
-	{"🌡", "sensors | awk '/^temp1:/{print $2}'",				5,		0},
-
-	{"", "~/bin/statusbar/clock",						5,		0},
+	/*Icon*/	/*Command*/				/*Update Interval*/	/*Update Signal*/
+//	{"",		"~/.local/bin/statusbar/mpc",		1,			12},
+//	{"",		"$LOCAL_BIN_STATUSBAR/ethernet",	2,			4},
+//	{"",		"$LOCAL_BIN_STATUSBAR/wifi",		2,			5},
+//	{"",		"$LOCAL_BIN_STATUSBAR/internet",	1200,			7},
+//	{"",		"$LOCAL_BIN_STATUSBAR/cputemp",		2,			13},
+//	{"",		"$LOCAL_BIN_STATUSBAR/disk",		5,			16},
+//	{"",		"$LOCAL_BIN_STATUSBAR/mem",		2,			14},
+//	{"",		"$LOCAL_BIN_STATUSBAR/battery",		2,			6},
+//	{"",		"$LOCAL_BIN_STATUSBAR/feed",		0,			15},
+//	{"",		"$LOCAL_BIN_STATUSBAR/updates",		0,			2},
+//	{"",		"$LOCAL_BIN_STATUSBAR/brightness",	0,			11},
+//	{"",		"$LOCAL_BIN_STATUSBAR/volume",		2,			10},
+//	{"",		"$LOCAL_BIN_STATUSBAR/clock",		2,			1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim = '|';
+static char delim = ' ';
