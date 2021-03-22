57d790f96bf36973489d8f02d3c888218ea9114c
Artur Dobrowolski <artur.dobrowolski@startpage.com>
2021-03-22 12:20:00 +0100

    Added statusbar-lowbat to statusbar

--- blocks.h
+++ blocks.h
@@ -1,17 +1,17 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/				/*Update Interval*/	/*Update Signal*/
-//	{"",		"~/.local/bin/statusbar/mpc",		1,			12},
-	{"",		"statusbar-ethernet",	2,			4},
-//	{"",		"statusbar-wifi",		2,			5},
-//	{"",		"statusbar-internet",	1200,			7},
-//	{"",		"statusbar-cputemp",		2,			13},
+	{"",		"statusbar-internet",		5,			17},
+	{"",		"statusbar-ethernet",		2,			4},
+	{"",		"statusbar-wifi",		2,			5},
+	{"",		"statusbar-cputemp",		2,			13},
 	{"",		"statusbar-disk",		5,			16},
 	{"",		"statusbar-memory",		2,			14},
-//	{"",		"statusbar-battery",		2,			6},
+	{"",		"statusbar-battery",		2,			6},
+	{"",		"statusbar-lowbat",		300,			18},
 	{"",		"statusbar-feed",		0,			15},
 	{"",		"statusbar-updates",		0,			2},
-//	{"",		"statusbar-brightness",	0,			11},
+	{"",		"statusbar-brightness",		0,			11},
 	{"",		"statusbar-volume",		2,			10},
 	{"",		"statusbar-clock",		2,			1},
 };
